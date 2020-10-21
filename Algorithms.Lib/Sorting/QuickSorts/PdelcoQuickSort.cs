22122154efa7605accf6edf96dd8ed2623ae4bcd
pdelcogliano <pdelco@hotmail.com>
2020-10-21 00:34:39 -0400

    added quicksort functionality and unit tests

---
+++ Algorithms.Lib/Sorting/QuickSorts/PdelcoQuickSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Algorithms.Lib.Sorting.QuickSorts
+{
+    public static class PdelcoQuickSort<T> where T: IComparable<T>
+    {
+        public static void QuickSort(T[] items, int start, int end)
+        {
+            if (start < end)
+            {
+                Split(items, start, end,  out int splitPart1, out int splitPart2);
+                if (splitPart1 < end)
+                    QuickSort(items, splitPart1, end);
+
+                if (start < splitPart2)
+                    QuickSort(items, start, splitPart2);
+            }
+        }
+
+        private static void Split(T[] items, int start, int end, out int splitPart1, out int splitPart2)
+        {
+            T element = items[(start + end) / 2];
+
+            do
+            {
+                while (items[start].CompareTo(element) < 0)
+                    start++;
+
+                while (items[end].CompareTo(element) > 0)
+                    end--;
+
+                if (start <= end)
+                {
+                    Swap(ref items[start], ref items[end]);
+                    start++;
+                    end--;
+                }
+            }
+            while (start < end);
+
+            splitPart1 = start;
+            splitPart2 = end;
+        }
+
+        private static void Swap(ref T element1, ref T element2)
+        {
+            T temp = element1;
+            element1 = element2;
+            element2 = temp;
+        }
+    }
+
+}
---
+++ DataStructuresAndAlgorithms.Test/AlgorithmTests/Sorting/QuickSorts/PdelcoQuickSortTests.cs
@@ -0,0 +1,63 @@
+﻿using Algorithms.Lib.Sorting.QuickSorts;
+using Xunit;
+
+namespace DataStructuresAndAlgorithms.Test.AlgorithmTests.Sorting.QuickSorts
+{
+    public class PdelcoQuickSortTests
+    {
+        [Fact]
+        public void PdelcoQuickSortedIntArray_Equals_ExpectedSortedIntArray()
+        {
+            int[] arr = { 350, 78, 5, 4, 80, 65, 17, 1, 9, 53 };
+            int[] expected = { 1, 4, 5, 9, 17, 53, 65, 78, 80, 350 };
+
+            PdelcoQuickSort<int>.QuickSort(arr, 0, arr.Length - 1);
+
+            Assert.Equal(expected, arr);
+        }
+
+        [Fact]
+        public void PdelcoQuickSortedIntArray_Equals_ExpectedSortedCharMixedCaseArray()
+        {
+            char[] arr = { 's', 'D', 'l', 'P', 'V', 'E', 'a', 'F', 'U', 'C' };
+            char[] expected = { 'C', 'D', 'E', 'F', 'P', 'U', 'V', 'a', 'l', 's' };
+
+            PdelcoQuickSort<char>.QuickSort(arr, 0, arr.Length - 1);
+
+            Assert.Equal(expected, arr);
+        }
+
+        [Fact]
+        public void PdelcoQuickSortedIntArray_Equals_ExpectedSortedCharLowerCaseArray()
+        {
+            char[] arr = { 's', 'd', 'l', 'p', 'v', 'e', 'a', 'f', 'u', 'c' };
+            char[] expected = { 'a', 'c', 'd', 'e', 'f', 'l', 'p', 's', 'u', 'v' };
+
+            PdelcoQuickSort<char>.QuickSort(arr, 0, arr.Length - 1);
+
+            Assert.Equal(expected, arr);
+        }
+
+        [Fact]
+        public void PdelcoQuickSortedIntArray_Equals_ExpectedSortedCharUpperCaseArray()
+        {
+            char[] arr = { 'S', 'D', 'L', 'P', 'V', 'E', 'A', 'F', 'U', 'C' };
+            char[] expected = { 'A', 'C', 'D', 'E', 'F', 'L', 'P', 'S', 'U', 'V' };
+
+            PdelcoQuickSort<char>.QuickSort(arr, 0, arr.Length - 1);
+
+            Assert.Equal(expected, arr);
+        }
+
+        [Fact]
+        public void PdelcoQuickSortedIntArray_Equals_ExpectedSortedStringArray()
+        {
+            string[] arr = { "abc", "ced", "xyz", "mno", "ghi", "def", "pqr", "bcd", "stu", "jkl" };
+            string[] expected = { "abc", "bcd", "ced", "def", "ghi", "jkl", "mno", "pqr", "stu", "xyz" };
+
+            PdelcoQuickSort<string>.QuickSort(arr, 0, arr.Length - 1);
+
+            Assert.Equal(expected, arr);
+        }
+    }
+}
